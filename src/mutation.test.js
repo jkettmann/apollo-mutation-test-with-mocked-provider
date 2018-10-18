fb3418ad2a7fb6a1d9afdee964ac6b71343fd02e
Johannes Kettmann <jkettmann@gmx.net>
2018-10-18 12:38:28 +0200

    Some cleanup

--- src/mutation.test.js
+++ src/mutation.test.js
@@ -25,13 +25,12 @@ const ADD_TODO_MUTATION = gql`
 const Component = () => (
   <Query query={TODOS_QUERY}>
     {
-      ({ data }) => {
-        console.log('render'); return (
+      ({ data }) => (
         <Mutation mutation={ADD_TODO_MUTATION}>
           {
             addTodo => (
               <div>
-                <button onClick={() => { console.log('addTodo'); addTodo({ variables: { title: 'My new todo' } }) }}>
+                <button onClick={() => addTodo({ variables: { title: 'My new todo' } })}>
                   Click me to add a todo!
                 </button>
 
@@ -46,13 +45,13 @@ const Component = () => (
             )
           }
         </Mutation>
-      )}
+      )
     }
   </Query>
 );
 
 
-it('renders without crashing', async () => {
+it('should render two todos after executing the mutation', async () => {
   const mocks = [
     {
       request: {
@@ -92,17 +91,16 @@ it('renders without crashing', async () => {
   await wait(100);
   wrapper.update();
 
-  console.log(wrapper.debug())
-
+  // only one todo is there as expected, tests are passing
   expect(wrapper.contains('An old todo')).toBe(true);
   expect(wrapper.contains('My new todo')).toBe(false);
-  wrapper.find('button').simulate('click');
 
+  // execute ADD_TODO_MUTATION and wait for render
+  wrapper.find('button').simulate('click');
   await wait(100);
   wrapper.update();
 
-  console.log(wrapper.debug())
-
+  // here I want both todos to be rendered but "My new todo" is not there
   expect(wrapper.contains('An old todo')).toBe(true);
   expect(wrapper.contains('My new todo')).toBe(true);
 });
